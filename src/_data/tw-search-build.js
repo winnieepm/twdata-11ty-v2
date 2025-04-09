f9454eb324832284546ebd7541aab4b576a54299
winnieepm <71612467+winnieepm@users.noreply.github.com>
2025-04-09 15:14:01 -0400

    adds progress comments

--- src/_data/tw-search-build.js
+++ src/_data/tw-search-build.js
@@ -5,13 +5,14 @@ const tweetCont = require("./tweets.json");
 const twid = require("./twids.json");
 const user = require("./users.json");
 
-// the following code works as expected when running `node script-name.js`
+// the following code works as expected when running `node script-name.js`. it creates an array in json format with the selected data from _data/
 const tweetData = Object.values(tweetCont).map(values => ({
     id:`${values.id}`,
     likes: `${values.favorite_count}`,
     text: `${values.text}`
 }));
 
+// still am not sure if this is working or where to "see" the search results.
 const lunrIndex = lunr(function () {
     this.ref('id');
     this.field('likes');
@@ -21,5 +22,7 @@ const lunrIndex = lunr(function () {
       this.add(doc)
     }, this)});
 
-  // console.log(tweetData)
-console.log(lunrIndex.search("the"));
+// console.log(tweetData) dev testing line 
+
+// the line below doesn't show search results. is this normal? where do i see them, then?
+// console.log(lunrIndex.search("the"));
